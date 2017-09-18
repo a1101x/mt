65b85a4fc96b6ddbe925a92246b067dc1c8f2bd2
AlexeyR <alexei_r@opengeekslab.com>
2017-09-18 15:29:39 +0300

    runall

--- runall.sh
+++ runall.sh
@@ -9,11 +9,11 @@ gnome-terminal 	--tab -e "bash -c \"source /home/a1101x/projects/mt/server_venv/
 		--tab -e "bash -c \"sleep 8; source /home/a1101x/projects/mt/sockets_venv/bin/activate; 
 			 cd /home/a1101x/projects/mt/mt_sockets/; pip install -r requirements.txt; 
 			 daphne apps.core.asgi:channel_layer --port 8002;\"" \
-		--tab -e "bash -c \"sleep 12; source /home/a1101x/projects/mt/sockets_venv/bin/activate; 
+		--tab -e "bash -c \"sleep 14; source /home/a1101x/projects/mt/sockets_venv/bin/activate; 
 			 cd /home/a1101x/projects/mt/mt_sockets/; python manage.py runworker;\"" \
-		--tab -e "bash -c \"sleep 14; source /home/a1101x/projects/mt/workers_venv/bin/activate; 
+		--tab -e "bash -c \"sleep 16; source /home/a1101x/projects/mt/workers_venv/bin/activate; 
 			 cd /home/a1101x/projects/mt/mt_workers/; 
 			 celery -A mt_workers worker --concurrency=4 -l info;\"" \
-		--tab -e "bash -c \"sleep 18; source /home/a1101x/projects/mt/workers_venv/bin/activate; 
+		--tab -e "bash -c \"sleep 20; source /home/a1101x/projects/mt/workers_venv/bin/activate; 
 			 cd /home/a1101x/projects/mt/mt_workers/; flower -A mt_workers --port=5555;\"" 
 
