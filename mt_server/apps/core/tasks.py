bc81fe1135ea8c3d8db3fc6ffc5d86b2524cae72
AlexeyR <alexei_r@opengeekslab.com>
2017-09-18 09:28:58 +0300

    runall:+pip install+migrate, cache timeout

--- mt_server/apps/core/tasks.py
+++ mt_server/apps/core/tasks.py
@@ -4,10 +4,9 @@ from django.contrib.auth.models import User
 from django.core.cache import cache
 from django.utils.crypto import get_random_string
 
-from celery import shared_task, task
+from celery import task
 
 
-# @shared_task
 @task(time_limit=30, default_retry_delay=60, max_retries=5)
 def create_random_user_accounts(total):
     try:
@@ -18,10 +17,9 @@ def create_random_user_accounts(total):
             User.objects.create_user(username=username, email=email, password=password)
 
             user = User.objects.get(username=username, email=email)
-            cache.set(user.id, user)
+            cache.set(user.id, user, timeout=3600)
 
             User.objects.get(username=username, email=email).delete()
         return '{} random users created with success!'.format(total)
     except User.DoesNotExist:
-        print("maybe do some clenup here....")
         create_random_user_accounts.retry()
--- mt_server/apps/core/views.py
+++ mt_server/apps/core/views.py
@@ -10,8 +10,8 @@ from .tasks import create_random_user_accounts
 class GenerateRandomUserView(View):
 
     def get(self, request, *args, **kwargs):
-        for i in range(10):
-            create_random_user_accounts.delay(100)        
+        for i in range(4):
+            create_random_user_accounts.delay(10)        
         return HttpResponse(json.dumps(
                                 {
                                     'response': 'We are generating your random users!'
